d7769750258d137ffd74fe62f1db4873e0597c97
Christine Wasike <christinewasike22@gmail.com>
2017-10-12 11:30:57 +0300

    Written code for user authentication with email in log in with email and password

--- app/src/main/java/com/example/wasike/lycitybot/MainActivity.java
+++ app/src/main/java/com/example/wasike/lycitybot/MainActivity.java
@@ -1,13 +1,132 @@
 package com.example.wasike.lycitybot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.icu.text.DateFormat;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static int SIGN_IN_REQUEST_CODE = 1;
+    private FirebaseListAdapter<ChatMessage> adapter;
+    RelativeLayout activty_main;
+    FloatingActionButton fab;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activty_main = (RelativeLayout) findViewById(R.id.activity_main);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText) findViewById(R.id.input);
+                FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessage(input.getText().toString(),
+                        FirebaseAuth.getInstance().getCurrentUser().getEmail()));
+                input.setText("");
+            }
+        });
+
+        //check if not signed in then navigate to SignIn Page
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_REQUEST_CODE );
+        } else {
+            Snackbar.make(activty_main, "Welcome" + FirebaseAuth.getInstance().getCurrentUser().getEmail(), Snackbar.LENGTH_SHORT)
+                    .show();
+        }
+
+        //Load content
+        displayChatMessage();
     }
+
+    private void displayChatMessage() {
+        ListView listOfMessages = (ListView) findViewById(R.id.list_of_messages);
+
+        adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
+                R.layout.list_item, FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                //Get references to the views of list_item.xml
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+
+                //Set their text
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                //Format te data before showing it
+
+                messageTime.setText("date here");
+            }
+        };
+        listOfMessages.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+
+            if(resultCode == RESULT_OK) {
+                Snackbar.make(activty_main, "Successfully signed in. Welcome!", Snackbar.LENGTH_SHORT)
+                        .show();
+                displayChatMessage();
+            } else {
+                Snackbar.make(activty_main, "We couldn't sign in. Please try again later.", Snackbar.LENGTH_SHORT)
+                        .show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_sign_out){
+            AuthUI.getInstance().signOut(this).
+                    addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(MainActivity.this, "You have been signed out.", Toast.LENGTH_LONG).show();
+
+                    //close activity
+                    finish();
+                }
+            });
+
+        }
+        return true;
+    }
+
 }
