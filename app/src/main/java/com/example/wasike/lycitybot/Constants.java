75781894eb5868a5e5b9bb819f36aa030e943b15
Okwonks <albertoketch@gmail.com>
2017-10-13 10:49:49 +0300

    Adding IBM Watson to the project as the AI to be used.

---
+++ app/src/main/java/com/example/wasike/lycitybot/Constants.java
@@ -0,0 +1,7 @@
+package com.example.wasike.lycitybot;
+
+public class Constants {
+    public static final String BLUEMIX_USER_NAME = BuildConfig.BLUEMIX_USER_NAME;
+    public static final String BLUEMIX_PASSWORD = BuildConfig.BLUEMIX_PASSWORD;
+    public static final String BLUEMIX_WORK_SPACEID = BuildConfig.BLUEMIX_WORK_SPACE_ID;
+}
---
+++ app/src/main/java/com/example/wasike/lycitybot/services/WatsonService.java
@@ -0,0 +1,8 @@
+package com.example.wasike.lycitybot.services;
+
+import com.example.wasike.lycitybot.Constants;
+import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+
+public class WatsonService {
+    public ConversationService watsonConversationService = new ConversationService("2017-05-06", Constants.BLUEMIX_USER_NAME, Constants.BLUEMIX_PASSWORD);
+}
