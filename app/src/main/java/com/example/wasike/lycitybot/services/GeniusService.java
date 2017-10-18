b4efa45fea3b7c159f1bbe29a65a9f4bd3b6ea57
Christine Wasike <christinewasike22@gmail.com>
2017-10-18 15:37:36 +0300

    Created a service responsible for ensuring a response fro the genius API is received

--- app/src/main/java/com/example/wasike/lycitybot/services/GeniusService.java
+++ app/src/main/java/com/example/wasike/lycitybot/services/GeniusService.java
@@ -1,8 +1,18 @@
 package com.example.wasike.lycitybot.services;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by wasike on 18/10/17.
  */
 
-public class GeniusService {
+public class GeniusService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
