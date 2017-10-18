45da06fc6e92b121a1fb2dcdc967d923e4804790
Christine Wasike <christinewasike22@gmail.com>
2017-10-18 15:54:56 +0300

    Seperated the constants necessary for the IBM watson to work as well as that of the Genius Api

--- app/src/main/java/com/example/wasike/lycitybot/Constants.java
+++ app/src/main/java/com/example/wasike/lycitybot/Constants.java
@@ -1,9 +1,13 @@
 package com.example.wasike.lycitybot;
 
 public class Constants {
+
+    //IBM Lexy Bot code is here
     public static final String BLUEMIX_USER_NAME = BuildConfig.BLUEMIX_USER_NAME;
     public static final String BLUEMIX_PASSWORD = BuildConfig.BLUEMIX_PASSWORD;
     public static final String BLUEMIX_WORK_SPACEID = BuildConfig.BLUEMIX_WORK_SPACE_ID;
     public static int SIGN_IN_REQUEST_CODE = 1;
     public static final String FIREBASE_CHILD_CHAT = "chat";
+
+    //The Genius API code is here
 }
