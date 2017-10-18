8fe755ae373508fd33d2d838816fea469ad689b4
Christine Wasike <christinewasike22@gmail.com>
2017-10-18 15:36:45 +0300

    Created the genius lyrics model that will have the models attributes as well as the getter and setter methods needed when making a request to the api

---
+++ app/src/main/java/com/example/wasike/lycitybot/models/Genius.java
@@ -0,0 +1,20 @@
+package com.example.wasike.lycitybot.models;
+
+/**
+ * Created by wasike on 18/10/17.
+ */
+
+public class Genius {
+    private String songTitle;
+    private String lyricsUrl;
+    private String artistName;
+    private String pushId;
+    private String imageThumbnail;
+
+    public Genius() {}
+
+    public Genius(String songTitle, String lyricsUrl, String artistName, String imageThumbnail){
+        this.songTitle = songTitle;
+        thi
+    }
+}
---
+++ app/src/main/java/com/example/wasike/lycitybot/services/GeniusService.java
@@ -0,0 +1,8 @@
+package com.example.wasike.lycitybot.services;
+
+/**
+ * Created by wasike on 18/10/17.
+ */
+
+public class GeniusService {
+}
