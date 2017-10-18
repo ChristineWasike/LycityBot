8bbc394b4011cf8e0cf8865223090bc6dcca1e24
Christine Wasike <christinewasike22@gmail.com>
2017-10-18 16:02:39 +0300

    Added the base url, genius api key and the genius query parameter constants to the Contanstants java class

--- app/src/main/java/com/example/wasike/lycitybot/Constants.java
+++ app/src/main/java/com/example/wasike/lycitybot/Constants.java
@@ -10,4 +10,13 @@ public class Constants {
     public static final String FIREBASE_CHILD_CHAT = "chat";
 
     //The Genius API code is here
+    public static final String GENIUS_API_KEY = BuildConfig.GENIUS_API_KEY;
+
+    //Base url to genius API here
+    public static final String GENIUS_BASE_URL = "https://api.genius.com/search?";
+
+    //Query for the request to song information based on the user's need
+    public static final String GENIUS_QUERY_PARAMETER = "q";
+
+
 }
