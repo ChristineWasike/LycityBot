d2ee26e035af55f479c4867c59108c3ad072a620
Christine Wasike <christinewasike22@gmail.com>
2017-10-30 08:41:24 +0300

    This java class is responsible for detecting that the app has been launch for the very first time by the user and hence the app intro is launched.

--- app/src/main/java/com/example/wasike/lycitybot/ui/PreferenceManager.java
+++ app/src/main/java/com/example/wasike/lycitybot/ui/PreferenceManager.java
@@ -1,5 +1,7 @@
 package com.example.wasike.lycitybot.ui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +9,34 @@ import com.example.wasike.lycitybot.R;
 
 public class PreferenceManager extends AppCompatActivity {
 
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context context;
+
+    //shared pref mode
+    int PRIVATE_MODE = 0;
+
+    //shared preferences file name
+    private static final String PREF_NAME = "intro_slider-welcome";
+
+    private static final String IS__FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PreferenceManager(Context context){
+        this.context = context;
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS__FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS__FIRST_TIME_LAUNCH, true);
+    }
+
+    //not sure if the onCreate will be used
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
