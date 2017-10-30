a85cc60f2bd5c0232eae4b6a51e1b9962e5ed48a
Christine Wasike <christinewasike22@gmail.com>
2017-10-30 08:54:59 +0300

    Created the LyricsActivity where the lyrics to the song will be displayed.

---
+++ app/src/main/java/com/example/wasike/lycitybot/ui/Lyrics.java
@@ -0,0 +1,15 @@
+package com.example.wasike.lycitybot.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.wasike.lycitybot.R;
+
+public class Lyrics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lyrics);
+    }
+}
