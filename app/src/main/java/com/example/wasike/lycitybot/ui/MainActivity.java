79bd38fc8ef347df4cd20ef9b2fb1a6ec1a0d5d3
Christine Wasike <christinewasike22@gmail.com>
2017-10-30 08:37:09 +0300

    Worked on how to display the Song artist upon search by user.

--- app/src/main/java/com/example/wasike/lycitybot/ui/MainActivity.java
+++ app/src/main/java/com/example/wasike/lycitybot/ui/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ListView;
